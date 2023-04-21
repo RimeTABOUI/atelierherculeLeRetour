f4375b6bd77688b0731e2aeab5ef44fa43c9f97d
rimeTABOU <rime.taboui@oclock.school>
2023-04-21 11:35:15 +0200

    etape 3

---
+++ js/exo.js
@@ -0,0 +1,14 @@
+console.log('test 1...2...1...2');
+
+let hercule = {
+    name: 'Hercule',
+    job: 'Demi-dieu',
+    age: 35,
+    department: 75,
+    arm: 60.5,
+    inRelationship: true,
+};
+
+base.fillProfil(hercule);
+
+
